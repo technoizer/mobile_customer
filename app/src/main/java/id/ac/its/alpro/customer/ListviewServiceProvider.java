61dc8f2d13b35095374bfc5677a6648b0703af91
cync48 <ridhoperdana@ymail.com>
2016-02-16 13:16:50 +0700

    custom listview provider

--- app/src/main/java/id/ac/its/alpro/customer/ListviewServiceProvider.java
+++ app/src/main/java/id/ac/its/alpro/customer/ListviewServiceProvider.java
@@ -7,15 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class PickService extends AppCompatActivity {
+public class ListviewServiceProvider extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_service);
+        setContentView(R.layout.activity_listview_service_provider);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/id/ac/its/alpro/customer/ProvidersDetil.java
+++ app/src/main/java/id/ac/its/alpro/customer/ProvidersDetil.java
@@ -7,15 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class PickService extends AppCompatActivity {
+public class ProvidersDetil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_service);
+        setContentView(R.layout.activity_providers_detil);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
